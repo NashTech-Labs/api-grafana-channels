7ae73cae0ec7bedbafc8320a90bc76c4ae4831c9
Vaibhav Kumar <vaibhav.kumar@knoldus.com>
2022-11-29 19:07:59 +0530

    added codes for techhub

---
+++ README.md
@@ -0,0 +1,8 @@
+## Using Grafana Api to acces grafana and apply CRUD actions
+This template helps you to create alerts and view them from your grafana server
+
+You can create API key using UI or follow the instructions of [Curl Commands Help](./curlAPIcommand.md)
+
+Then use 
+    1. [View](./getnotificationchannelsInfo.py) : to view the available alert rules
+    2. [Create](./createnotificationchannel.py) : to add new rules
---
+++ createnotificationchannel.py
@@ -0,0 +1,47 @@
+import requests
+import json
+
+grafana_url = "a5c07bf2eeabc479f99071aaddbac98b-364804466.ap-south-1.elb.amazonaws.com"
+username = "admin"
+password = "prom-operator"
+
+base_url = "http://{}:{}@{}".format(username, password, grafana_url)
+
+headers = {
+    'Authorization': 'Bearer eyJrIjoiN0xNM1V4UWpMUkVoZXRKaWh2bVJya3IzZGNqb0dvTGkiLCJuIjoiQWRtaW4iLCJpZCI6MX0=',
+    'Content-Type': 'application/json',
+    'Accept': "application/json"
+}
+
+json_data = {
+  "Name": "slack-alerts",  
+  "Type":  "slack",
+  "sendReminder": True,
+  "frequency" : "15m",
+  "settings": {
+    "url" : "https://hooks.slack.com/services/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",
+    "Username":"devops1-bot",
+    "recipient":"#monitoring",
+    "Icon emoji":":smile:",
+    "Icon Url":"https://avatars.githubusercontent.com/u/7195757",
+    "token":"123"
+  }
+}
+
+#for iconurl just search https://github.com/grafana.png
+# {
+#   "uid": "new-alert-notification", // optional
+#   "name": "new alert notification",  //Required
+#   "type":  "email", //Required
+#   "isDefault": false,
+#   "sendReminder": false,
+#   "settings": {
+#     "addresses": "dev@grafana.com"
+#   }
+# }
+
+
+resp = requests.post(base_url + "/api/v1/provisioning/contact-points",headers=headers, json=json_data, verify=False)
+# resp = requests.post(base_url + "/api/alert-notifications",headers=headers, json=json_data, verify=False) this is for legacy
+print(json.dumps(resp.json(), indent=4))
+
